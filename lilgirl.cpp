6a1e6278a57a9fcf335382ee72dd595530a634b9
Bighnesh Kumar Pati <bighneshmacbook@Bighneshs-MacBook-Air.local>
2021-07-20 17:16:06 +0530

    added little girl and maximum sum

---
+++ lilgirl.cpp
@@ -0,0 +1,40 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define int long long
+int32_t main()
+{
+   ios::sync_with_stdio(false);
+   cin.tie(0);
+   #ifndef ONLINE_JUDGE
+   freopen("input.txt", "r", stdin);
+   freopen("output.txt", "w", stdout);
+   #endif
+   int n,q;
+   cin>>n>>q;
+   vector<int> arr(200500);
+   for(int i=0;i<n;i++){
+       cin>>arr[i];
+   }
+    vector<int> cnt(200500,0);
+   while(q--){
+       int l,r;
+       cin>>l>>r;
+       cnt[l-1]++;
+       cnt[r]--;
+   }
+   for(int i=1;i<n;i++){
+       cnt[i] += cnt[i-1];
+   }
+   vector<int> v;
+    for(int i=0;i<n;i++){
+        v.push_back(cnt[i]);
+    }
+    sort(v.begin(),v.end());
+    sort(arr.begin(),arr.begin()+n);
+    int res = 0;
+    for(int i=0;i<n;i++){
+        res += arr[i]*v[i];
+    }
+    cout<<res;
+   return 0;
+}
