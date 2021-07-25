a65c21bc397e6eefd0d7df5f1aad074daf0dccaa
Bighnesh Kumar Pati <bighneshmacbook@Bighneshs-MacBook-Air.local>
2021-07-25 23:13:51 +0530

    codeforces global

--- runningforgold.cpp
+++ runningforgold.cpp
@@ -1,58 +1,98 @@
-#include "bits/stdc++.h"
+#include<bits/stdc++.h>
 using namespace std;
-const int maxn = 1e6 + 1;
 #define int long long
-bool cmp(pair<int,int> &p1,pair<int,int> &p2){
-    return p1.first>p2.first;
+#define M 1000000007
+#define INF ((1LL<<62LL) - 1)
+ 
+bool comp(vector<int>& a, vector<int>& b){
+    int cnt = 0;
+    for(int i=0;i<5;i++){
+        if(a[i] < b[i]){
+            cnt++;
+        }
+    }
+ 
+    return cnt >= 3;
 }
-int32_t main()
-{
-   ios::sync_with_stdio(false);
-   cin.tie(0);
-   #ifndef ONLINE_JUDGE
-   freopen("input.txt", "r", stdin);
-   freopen("output.txt", "w", stdout);
-   #endif
-   int t;
-   cin>>t;
-   while(t--){
-       int n;
-       cin>>n;
-       vector<vector<int>> arr(n+1,vector<int>(6));
-       for(int i=1;i<=n;i++){
-           for(int j=1;j<=5;j++){
-               cin>>arr[i][j];
-           }
-       }
-       vector<int> ath(n,0);
-       for(int j=1;j<=5;j++){
-           int win ;
-           int minm = 1e6;
-           for(int i=1;i<=n;i++){
-               if(arr[i][j]<minm){
-                   win = i;
-                    minm = arr[i][j];
-               }
-           }
-           ath[win-1]++;
-       }
-        vector<pair<int,int>> vec;
-        
-        for(int i=0;i<ath.size();i++){
-            vec.push_back(make_pair(ath[i],i+1));
+ 
+void calc(){
+    int n; cin >> n;
+    vector<int> a[n];
+    vector<int> b[n];
+ 
+    set<int> s;
+ 
+    for(int i=0;i<n;i++){
+        for(int j=0;j<5;j++) {
+            int x;
+            cin >> x;
+            a[i].push_back(x);
+            b[i].push_back(x);
         }
-        sort(vec.begin(),vec.end(),cmp);
-        // for(int i=0;i<ath.size();i++){
-        //     cout<<vec[i].first<<" ";
-        // }
-        // cout<<endl;
-        if(n==1)
-            cout<<"1"<<endl;
-        else if(vec[0].first==vec[1].first)
-            cout<<"-1"<<endl;
-        else{
-            cout<<vec[0].second<<endl;
+        a[i].push_back(i);
+    }
+ 
+    sort(a, a+n, comp);
+ 
+    for(int i=1;i<n;i++){
+        if(!comp(a[0], a[i])){
+            cout << "-1\n";
+            return;
         }
-   }
-   return 0;
+    }
+ 
+    cout << a[0].back()+1 << "\n";
+ 
+//    for(int j=0;j<5;j++) {
+//        vector<pair<int,int> > vp;
+//        for (int i = 0; i < n; i++) {
+//            vp.push_back({a[i][j], i});
+//        }
+//
+//        sort(vp.begin(), vp.end());
+//        for(int i=0;i<min(10LL, (int)vp.size());i++){
+//            s.insert(vp[i].second);
+//        }
+//    }
+ 
+//    for(int x : s){
+//        bool ok = true;
+//        for(int i=0;i<n;i++){
+//            if(i == x){
+//                continue;
+//            }
+//            if(!comp(a[x], a[i])){
+//                ok = false;
+//                break;
+//            }
+//        }
+//        if(ok){
+//            cout << x+1 << "\n";
+//            return;
+//        }
+//    }
+//
+//    cout << "-1\n";
+ 
+ 
+ 
+}
+ 
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    int t;
+    cin>>t;
+    while(t--){
+        calc();
+    }
+ 
+ 
+ 
+    
+ 
 }
