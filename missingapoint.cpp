022ad89d291ef26a0683791ed57437a8b611b91c
Bighnesh Kumar Pati <bighneshmacbook@Bighneshs-MacBook-Air.local>
2021-08-08 23:01:05 +0530

    added missing a point from codeforces

---
+++ missingapoint.cpp
@@ -0,0 +1,32 @@
+#include "bits/stdc++.h"
+using namespace std;
+const int maxn = 1e6 + 1;
+#define int long long
+int32_t main()
+{
+   ios::sync_with_stdio(false);
+   cin.tie(0);
+   #ifndef ONLINE_JUDGE
+   freopen("input.txt", "r", stdin);
+   freopen("output.txt", "w", stdout);
+   #endif
+   int t;
+   cin>>t;
+   while(t--){
+       int n;
+       cin>>n;
+       vector<int>x(4*n-1);
+       vector<int>y(4*n-1);
+       for(int i=0;i<4*n-1;i++){
+           cin>>x[i]>>y[i];
+       }
+       int val1 = 0;
+       int val2 = 0;
+       for(int i=0;i<x.size();i++){
+           val1 ^= x[i];
+           val2 ^= y[i];
+       }
+       cout<<val1<<" "<<val2<<endl;
+   }
+   return 0;
+}
