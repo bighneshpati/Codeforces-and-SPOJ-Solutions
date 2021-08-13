871a5ae9e30b2d3d806437f6e516b718ecb378e5
Bighnesh Kumar Pati <bighneshmacbook@Bighneshs-MacBook-Air.local>
2021-08-13 11:12:37 +0530

    Box packing added

---
+++ boxespacking.cpp
@@ -0,0 +1,27 @@
+#include "bits/stdc++.h"
+using namespace std;
+const int maxn = 1e6 + 1;
+#define int long long
+int32_t main()
+{
+   ios::sync_with_stdio(false);
+   cin.tie(0);
+   #ifndef ONLINE_JUDGE
+   freopen("input.txt", "r", stdin);
+   freopen("output.txt", "w", stdout);
+   #endif
+   int n;
+   cin>>n;
+   unordered_map<int,int> umap;
+   for(int i=0;i<n;i++){
+       int temp;
+       cin>>temp;
+       umap[temp]++;
+   }
+   int ans = 0;
+   for(auto i: umap){
+       ans = max(ans,i.second);
+   }
+   cout<<ans<<endl;
+   return 0;
+}
